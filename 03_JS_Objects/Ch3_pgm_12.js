638b9de2870624556dcebc308ac096c2abc90091
2226j1544 <144420790+2226j1544@users.noreply.github.com>
2024-07-14 14:55:12 +0530

    Update Ch3_pgm_12.js

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -17,3 +17,24 @@ var post = {
  * 2) Add a second post.
  *
  */
+console.log("Post ID: " + post.id);
+console.log("Title: " + post.title);
+console.log("Author: " + post.author);
+console.log("Created Date: " + post.created);
+console.log("Body: " + post.body);
+
+
+var post2 = {
+    id: 2,
+    title: "Harry potter and philosopher",
+    author: "J.K.Rowling",
+    created: "1997",
+    body: "It gives the real meaning of life."
+};
+
+
+console.log("Post ID: " + post2.id);
+console.log("Title: " + post2.title);
+console.log("Author: " + post2.author);
+console.log("Created Date: " + post2.created);
+console.log("Body:" + post2.body);
