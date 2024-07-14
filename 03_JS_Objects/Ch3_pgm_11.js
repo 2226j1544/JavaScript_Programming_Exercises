6e7f2ce59fb27edce20c900c03eb6004a7e94c95
2226j1544 <144420790+2226j1544@users.noreply.github.com>
2024-07-14 14:48:57 +0530

    Update Ch3_pgm_11.js

--- 03_JS_Objects/Ch3_pgm_11.js
+++ 03_JS_Objects/Ch3_pgm_11.js
@@ -27,3 +27,16 @@ console.log(player1.name + " has scored " + player1.score);
  *    their names.
  *
  */
+player1.score = player1.score * 1.1; 
+
+console.log(player1.name + "'s score after 10% increase: " + player1.score);
+
+
+var player2 = {
+    name: "John",
+    score: 100
+};
+
+
+var totalScore = player1.score + player2.score;
+console.log(player1.name + " and " + player2.name + " have a total score of " + totalScore);
